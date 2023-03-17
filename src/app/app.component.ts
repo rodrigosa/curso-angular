2f15b613deb6cb9cd456212a9963126be0f424ac
Rodrigo de Oliveira Sá <70106798@amcontratos.com.br>
2023-03-17 14:15:39 -0300

    Aula 14 (OnChange)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,15 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<router-outlet></router-outlet>'
+  template: `
+  <app-title title="Olá Mundo"></app-title>
+  <router-outlet></router-outlet>
+  `
+  
+  
 })
 export class AppComponent implements OnInit {
   constructor() {}
   
-  ngOnInit(): void {
-      
-  }
-}
+  ngOnInit(): void { }
+}  
